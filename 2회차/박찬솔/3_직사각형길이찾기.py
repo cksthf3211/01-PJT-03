211c2565db418ede1136e0cd949dc05a12460a9a
cksthf3211 <cksthf321@gmail.com>
2022-07-29 18:03:38 +0900

    프로잭트

--- 2회차/박찬솔/3_직사각형길이찾기.py
+++ 2회차/박찬솔/3_직사각형길이찾기.py
@@ -1,3 +1,9 @@
 import sys
 
 sys.stdin = open("_직사각형길이찾기.txt")
+# 사각형의 네 변 중에서 세 변의 길이가 주어짐
+# 나머지 한 변의 길이가 얼마인지 출력하는 프로그램을 작성
+T = map(int, input().split())
+for i in range():
+
+    print(f'#{}{}')
